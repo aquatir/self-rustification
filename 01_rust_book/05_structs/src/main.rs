ea58a0a2a7c3b684b5c7226bf848eb37e3436965
pavel bukhmatov <buhmatov@gmail.com>
2026-08-12 14:05:02 +0400

    add struct emaples + 06 enums pattern matching

--- 01_rust_book/05_structs/src/main.rs
+++ 01_rust_book/05_structs/src/main.rs
@@ -9,17 +9,50 @@ fn main() {
     println!("{separator}");
     println!("tuple structs");
     tuple_structs();
+
+    println!("{separator}");
+    println!("methods");
+    methods();
+}
+
+fn methods() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+    dbg!(&rect1);
+    dbg!(rect1.area());
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(50);
+    dbg!(&square);
 }
 
 fn tuple_structs() {
     let black = Color(0, 0, 0);
     let origin = Point(0, 0, 0);
 
-    println!("{:?}", black); // Color(0, 0, 0)
-    println!("{:?}", origin); // Point(0, 0, 0)
+    println!("{black:?}"); // Color(0, 0, 0)
+    println!("{origin:?}"); // Point(0, 0, 0)
 
     let subject = AlwaysEqual;
-    println!("{:?}", subject);
+    println!("{subject:?}");
 }
 
 struct Color(i32, i32, i32);
@@ -97,5 +130,33 @@ impl fmt::Display for User {
             "{} ({}) active '{}' sing_in_count '{}'",
             self.username, self.email, self.active, self.sign_in_count
         )
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        return Self {
+            width: size,
+            height: size,
+        };
+    }
+
+    // &self is a short form for self: &Self, where Self is a type in impl,
+    // so self: &Rectangle is also the same
+
+    // fn area(self: &Rectangle) -> u32 {
+    // fn area(self: &Self) -> u32 {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        return self.width >= other.width && self.height >= other.width;
     }
 }
